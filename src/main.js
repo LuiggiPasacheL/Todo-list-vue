8ae5be3feddfbbff777253fc266c32cef06a4ee6
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2023-02-25 11:32:00 -0500

    Add bootstrap-vue-next library

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import BootstrapVueNext from 'bootstrap-vue-next'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+const app = createApp(App)
+app.use(BootstrapVueNext)
+app.mount('#app')
